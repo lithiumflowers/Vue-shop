f0e0cf0ef919c57c991a62c5a83e2dc5e32eda53
lithiumflowers <danhangbu@outlook.com>
2022-07-31 14:21:23 +0800

    feat: 静态登录页面

---
+++ src/components/SvgIcon/index.vue
@@ -0,0 +1,29 @@
+<template lang="">
+  <svg class="svg-icon" aria-hidden="true">
+    <use :xlink:href="IconName"></use>
+  </svg>
+</template>
+
+<script setup>
+import { defineProps, computed } from 'vue'
+const props = defineProps({
+  icon: {
+    type: String,
+    required: true
+  }
+})
+
+const IconName = computed(() => {
+  return `#icon-${props.icon}`
+})
+</script>
+
+<style lang="scss" scoped>
+.svg-icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
